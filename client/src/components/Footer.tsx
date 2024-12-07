1d6122240529484e0bc2e1fc70f6179b05fb2fe4
Vighnesh Brahme <v8ganesh@gmail.com>
2024-12-07 09:21:35 +0530

    made footer responsive, add lucide icons, useMediaQuery hook

--- client/src/components/Footer.tsx
+++ client/src/components/Footer.tsx
@@ -1,24 +1,30 @@
+import useMediaQuery from "@/lib/useMediaQuery";
+import { Github, Mail, Twitter } from "lucide-react";
+
 export default function Footer() {
+  const isMobile = useMediaQuery("(max-width: 768px)");
+
   return (
     <footer className="absolute bottom-0 left-0 right-0 w-full footer text-white pb-4 pt-4">
-      <ul className="list-none flex gap-10 justify-center items-center">
+      <ul className="list-none flex gap-4 sm:gap-10 justify-center items-center">
         <li>
           <a
             target="_blank"
             rel="noopener noreferrer"
             href="https://www.github.com/mendacium-a11y/Shortify"
           >
-            Github
+            {isMobile ? <Github /> : "Github"}
           </a>
         </li>
         <li>
           <a target="_blank" href="https://twitter.com/Saksham2467">
-            Twitter
+            {isMobile ? <Twitter /> : "Twitter"}
+            
           </a>
         </li>
         <li>
           <a target="_blank" href="mailto:sakshamsaha1212+shortify@gmail.com">
-            E-mail
+            {isMobile ? <Mail /> : "E-mail"}
           </a>
         </li>
         <li>
