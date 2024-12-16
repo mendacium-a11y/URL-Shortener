a447bb2246dd2c2b7aac58a3686da42c19570486
Archi Shaw <archishaw2004@gmail.com>
2024-12-16 12:38:02 +0530

    Resolved

--- server/.eslintrc.cjs
+++ server/.eslintrc.cjs
@@ -1,20 +1,20 @@
 module.exports = {
-  env: {
-      node: true,
-      es2021: true,
-  },
-  extends: [
-      "standard",
-  ],
-  parserOptions: {
-      ecmaVersion: "latest",
-      sourceType: "module",
-  },
-  rules: {
-      "no-useless-escape": "off",
-      "indent": ["error", 4],
-      "no-console": "off",
-      "@typescript-eslint/no-unused-vars": "off",
-      "consistent-return": "off",
-  },
+    env: {
+        node: true,
+        es2021: true,
+    },
+    extends: [
+        'standard',
+    ],
+    parserOptions: {
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+    },
+    rules: {
+        semi: ['error', 'always'],
+        'comma-dangle': ['error', 'only-multiline'],
+        noUselessEscape: 'off',
+        indent: ['error', 4],
+        noConsole: 'off'
+    }
 };
